8ef3b28f8841e20e67b85a6f1604132277d41a81
김주원 <56067949+Kim-Juwon@users.noreply.github.com>
2023-12-19 21:23:15 +0900

    Update 1103.java

--- baekjoon/1103/1103.java
+++ baekjoon/1103/1103.java
@@ -1,75 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-// 참고: https://steady-life.tistory.com/67 
-
+/*
+     지식
+     - 무한번 움직인다는 것은 순환이 발생했다는 것이다.
+     - 기저 조건에서 0을 반환하고, dfs() 호출 결과값에 +1 하여 memo에 저장
+     주의할 점
+     - '#' 입력이 들어오므로 char 2차원 배열로 저장해야 함
+ */
 public class Main {
-
-    static int N, M, answer = 0;
-    static int[][] map;
-    static boolean[][] visit;
-    static int[][] dp;
-
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer stringTokenizer;
+    static int N, M;
+    static char[][] matrix;
+    static boolean[][] visited;
+    static int[][] memo;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        stringTokenizer = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        M = Integer.parseInt(stringTokenizer.nextToken());
 
-        String[] nm = br.readLine().split(" ");
-        N = Integer.parseInt(nm[0]);
-        M = Integer.parseInt(nm[1]);
-
-        map = new int[N][M];
-        visit = new boolean[N][M];
-        dp = new int[N][M];
+        matrix = new char[N][M];
+        visited = new boolean[N][M];
+        memo = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-
+            matrix[i] = toCharArray(reader.readLine());
             for (int j = 0; j < M; j++) {
-                dp[i][j] = -1;
-
-                if (str.charAt(j) == 'H') {
-                    map[i][j] = 0;
-                } else {
-                    map[i][j] = str.charAt(j) - '0';
-                }
+                memo[i][j] = -1;
             }
         }
 
-        answer = DFS(0, 0);
-
-        System.out.println(answer);
+        writer.write(String.format("%d\n", dfs(0, 0)));
+        writer.flush();
+        writer.close();
     }
 
-    static int DFS(int x, int y) {
-        if (x < 0 || y < 0 || x >= N || y >= M || map[x][y] == 0) {
+    static char[] toCharArray(String str) {
+        char[] charArray = new char[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            charArray[i] = str.charAt(i);
+        }
+
+        return charArray;
+    }
+
+    static int dfs(int currentRow, int currentCol) throws IOException {
+        if (!isValidLocation(currentRow, currentCol)) {
             return 0;
         }
-
-        if (visit[x][y]) {
-            System.out.println(-1);
+        if (visited[currentRow][currentCol]) {
+            writer.write("-1\n");
+            writer.flush();
+            writer.close();
             System.exit(0);
         }
-
-        if (dp[x][y] != -1) {
-            return dp[x][y];
+        if (memo[currentRow][currentCol] != -1) {
+            return memo[currentRow][currentCol];
         }
 
-        visit[x][y] = true;
-        dp[x][y] = 0;
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, -1, 0, 1};
+        int distance = Character.getNumericValue(matrix[currentRow][currentCol]);
 
+        visited[currentRow][currentCol] = true;
         for (int i = 0; i < 4; i++) {
-            int nx = x + (map[x][y] * dx[i]);
-            int ny = y + (map[x][y] * dy[i]);
+            int nextRow = currentRow + dRow[i] * distance;
+            int nextCol = currentCol + dCol[i] * distance;
 
-            dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
+            memo[currentRow][currentCol] = Math.max(memo[currentRow][currentCol], dfs(nextRow, nextCol) + 1);
         }
+        visited[currentRow][currentCol] = false;
 
-        visit[x][y] = false;
+        return memo[currentRow][currentCol];
+    }
 
-        return dp[x][y];
+    static boolean isValidLocation(int row, int col) {
+        return row >= 0 && row < N && col >= 0 && col < M && matrix[row][col] != 'H';
     }
 }
