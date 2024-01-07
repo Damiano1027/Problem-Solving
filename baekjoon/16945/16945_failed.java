3dcfa20182e9e3f8ca13b1425f013b97e06aa708
김주원 <56067949+damiano1027@users.noreply.github.com>
2024-01-07 20:21:34 +0900

    Create 16945_failed.java

---
+++ baekjoon/16945/16945_failed.java
@@ -0,0 +1,99 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer tokenizer;
+    static int[][] matrix = new int[3][3];
+    static int result = Integer.MAX_VALUE;
+
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 3; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = Integer.parseInt(tokenizer.nextToken());
+            }
+        }
+
+        solve();
+
+        writer.write(String.format("%d\n", result));
+        writer.flush();
+        writer.close();
+    }
+
+    static void solve() {
+        dfs(new ArrayList<>());
+    }
+
+    static void dfs(List<Integer> numbers) {
+        if (numbers.size() >= 9) {
+            int[][] matrix = toMatrix(numbers);
+            if (isMagicSquare(matrix)) {
+                result = Math.min(result, cost(matrix));
+            }
+            return;
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            numbers.add(i);
+            dfs(numbers);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
+
+    static int[][] toMatrix(List<Integer> numbers) {
+        int[][] matrix = new int[3][3];
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrix[i][j] = numbers.get(index++);
+            }
+        }
+
+        return matrix;
+    }
+
+    static boolean isMagicSquare(int[][] matrix) {
+        int diagonal1Sum = matrix[0][0] + matrix[1][1] + matrix[2][2];
+        int diagonal2Sum = matrix[0][2] + matrix[1][1] + matrix[2][0];
+
+        if (diagonal1Sum != diagonal2Sum) {
+            return false;
+        }
+
+        for (int row = 0; row < 3; row++) {
+            int rowSum = matrix[row][0] + matrix[row][1] + matrix[row][2];
+            if (rowSum != diagonal1Sum) {
+                return false;
+            }
+        }
+        for (int col = 0; col < 3; col++) {
+            int colSum = matrix[0][col] + matrix[1][col] + matrix[2][col];
+            if (colSum != diagonal1Sum) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static int cost(int[][] magicSquareMatrix) {
+        int cost = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] != magicSquareMatrix[i][j]) {
+                    cost += Math.abs(matrix[i][j] - magicSquareMatrix[i][j]);
+                }
+            }
+        }
+
+        return cost;
+    }
+}
