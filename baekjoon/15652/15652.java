81c6b33e3fa808f0b5057dca7de7f1cf584875c5
김주원 <56067949+damiano1027@users.noreply.github.com>
2024-05-01 20:43:00 +0900

    Create 15652.java

---
+++ baekjoon/15652/15652.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int N, M;
+    static StringTokenizer tokenizer;
+
+    public static void main(String[] args) throws Exception {
+        tokenizer = new StringTokenizer(reader.readLine());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
+
+        solve();
+
+        writer.flush();
+        writer.close();
+    }
+
+    static void solve() throws Exception {
+        List<Integer> numbers = new ArrayList<>();
+        for (int number = 1; number <= N; number++) {
+            numbers.add(number);
+            dfs(numbers);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
+
+    static void dfs(List<Integer> numbers) throws Exception {
+        if (numbers.size() == M) {
+            for (int number : numbers) {
+                writer.write(number + " ");
+            }
+            writer.newLine();
+            return;
+        }
+
+        int currentNumber = numbers.get(numbers.size() - 1);
+
+        for (int nextNumber = currentNumber; nextNumber <= N; nextNumber++) {
+            numbers.add(nextNumber);
+            dfs(numbers);
+            numbers.remove(numbers.size() - 1);
+        }
+    }
+}
